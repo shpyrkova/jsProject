8e7afe6f30cd0fcf598cc665531cc600a6819470
Dasha <moe_more@list.ru>
2020-09-18 03:09:29 +0300

    lesson14

---
+++ js/14lesson.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// Циклы  
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Guide/%D0%A6%D0%B8%D0%BA%D0%BB%D1%8B_%D0%B8_%D0%B8%D1%82%D0%B5%D1%80%D0%B0%D1%86%D0%B8%D0%B8
+
+let num = 50;
+
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
+
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        // break; break - выходит из цикла 
+        continue; // исключает указанное условие (не делает следующее действие), но проодолжает цикл дальше
+    }
+    console.log(i);
+}
