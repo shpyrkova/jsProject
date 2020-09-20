5dc679e867f2dd5e7f711b8cc5eb1e57e703a085
Dasha <moe_more@list.ru>
2020-09-20 21:32:47 +0300

    app part2

--- first_app/app.js
+++ first_app/app.js
@@ -1,6 +1,10 @@
 'use strict';
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '0');
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+if (numberOfFilms == null || numberOfFilms == "") {
+    prompt('Сколько фильмов вы уже посмотрели?', '');
+}
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,12 +14,28 @@ const personalMovieDB = {
     privat: false 
 };
 
-let lastFilmFirst = prompt('Один из последних просмотренных фильмов?', ''),
-    firstFilmRate = prompt('На сколько оцените его?', '0'),
-    lastFilmSecond = prompt('Один из последних просмотренных фильмов?', ''),
-    secondFilmRate = prompt('На сколько оцените его?', '0');
+    for (let i = 0; i < 2; i++) {
+        let f = prompt('Один из последних просмотренных фильмов?', ''),
+            r = prompt('На сколько оцените его?', '0');
+        if (f != "" && f != null && f.length < 50 && r != null && r != '') {
+            personalMovieDB.movies[f] = r;
+            console.log('done!');
+        }
+        else {
+            console.log('error');
+            i--;
+        }
+    }
 
-personalMovieDB.movies[lastFilmFirst] = firstFilmRate;
-personalMovieDB.movies[lastFilmSecond] = secondFilmRate;
+    // не можем использовать switch, потому что в нем только прямое строгое сравнение, а не > <   
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов!");
+} else if (10 <= personalMovieDB.count <= 30) {
+    alert("Вы классический зритель!");
+} else if (personalMovieDB.count > 30) {
+    alert("Вы киноман!");
+} else {
+    alert("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
